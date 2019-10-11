35cdc6130c6d2543c46a65e23d27885ab67ed318
Ali <ali.najmaldin@gmail.com>
2019-10-10 20:26:15 -0400

    finally works lmao

--- question3.py
+++ question3.py
@@ -12,6 +12,7 @@ def createPMatrix(dims):
     P = np.reshape(P, dims)
     return P
 
+
 # Correct
 def initialFill(S, P):
     dims = S.shape
@@ -37,7 +38,6 @@ def fillNext(pos):
     if seq1[y] == seq2[x]:
         opt1 = int(S[y-1, x-1] + scheme['match'])
 
-
     opts = [opt1, opt2, opt3]
     newScore = max(opts)
 
@@ -56,56 +56,30 @@ def fillNext(pos):
     P[y, x] = bestMove
 
 
-
 # Correct
 # fills out the whole grids of S and P
 def fillAll(dims):
-    for x in range(1, dims[0]):
-        for y in range(1, dims[1]):
+    for x in range(1, dims[1]):
+        for y in range(1, dims[0]):
             fillNext((x, y))
-
-
-# # TODO multiple solutions fix
-# # def tracer(dims):
-# #     # remove 1 cuz length is mag
-# #     dimY = dims[0]-1
-# #     dimX = dims[1]-1
-# #
-# #     allign = str(seq1[dimY])
-# #     print(dimY, dimX, P.shape)
-# #     print(P[16, 14])
-# #     while dimY > 0 and dimX > 0:
-# #         if P[dimY, dimX] == 'D':
-# #             dimX = dimX -1
-# #             dimY = dimY -1
-# #             allign = allign + str(seq2[dimX])
-# #         elif P[dimY, dimX] == 'H':
-# #             dimX = dimX - 1
-# #             allign = allign + str(seq2[dimX])
-# #         elif P[dimY, dimX] == 'V':
-# #             dimY = dimY - 1
-# #             allign = allign + str(seq2[dimX])
-# #         # dimX = dimX - 1
-# #         # dimY = dimY - 1
-# #     print(allign[::-1])
-
-
 
 
 # Initial sequence; seq2 sequence is row, second seq1 is col
 # Given sequence to align
 
-# seq1 = 'GAATACAGTTATGCTA'
-# seq2 = 'GGATGCGTGATCTG'
-seq1 = 'GATTACA'
-seq2 = 'GATAGA'
+seq2 = 'GAATACAGTTATGCTA'
+seq1 = 'GGATGCGTGATCTG'
+
+# transpose by putting seq 1 in 2
+# seq2 = 'GATTACA'
+# seq1 = 'GATAGA'
 
 seq1 = "-"+seq1
 seq2 = "-"+seq2
 # scheme = [1, 0, -1]
 # scheme = [3, -1, 0]
 scheme = {'match':1, 'miss':0, 'gap':-1}
-
+# scheme = {'match':3, 'miss':-1, 'gap':0}
 
 dims = (len(seq1), len(seq2))
 S = np.zeros(dims)
@@ -125,8 +99,8 @@ print(P)
 # Plot the figures
 fig, ax1 = plt.subplots(1,1)
 imgplot = ax1.imshow(S)
-# ax1.set_xticks(range(len(seq1)))
-# ax1.set_yticks(range(len(seq2)))
-# ax1.set_xticklabels(list(seq1))
-# ax1.set_yticklabels(list(seq2))
+ax1.set_xticks(range(len(seq2)))
+ax1.set_yticks(range(len(seq1)))
+ax1.set_xticklabels(list(seq2))
+ax1.set_yticklabels(list(seq1))
 plt.show()
